d3fc977b668801a156e393ad738fcd9011f36355
nasrullah <102759013+itsNas@users.noreply.github.com>
2024-07-12 18:55:16 +0800

    add organization

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,23 +1,45 @@
-﻿import { auth } from "@clerk/nextjs/server"
+﻿import { buttonVariants } from "@/components/ui/button"
+import { MessageState } from "@/components/dashboard/message-state"
+import { TitleBar } from "@/components/dashboard/title-bar"
 
-import { SessionDetails, UserDetails } from "./details"
-import { OnboardingDetails } from "./onboarding-details"
-
-export default function Dashboard() {
-  const { userId, sessionClaims } = auth()
+const DashboardIndexPage = () => {
+  const titleBar = "Dashboard Title" // Replace with your static title
+  const titleBarDescription = "This is the description of the title bar." // Replace with your static description
+  const messageStateTitle = "Message State Title" // Replace with your static message state title
+  const messageStateDescription = "This is the description of the message state." // Replace with your static message state description
+  const messageStateButton = "Learn More" // Replace with your static button text
 
   return (
-    <div className='px-8 py-12 sm:py-16 md:px-20'>
-      {userId && (
-        <>
-          <h1 className='text-3xl font-semibold'>👋 Hi, {sessionClaims?.firstName || `Stranger`}</h1>
-          <div className='mt-8 grid gap-4 lg:grid-cols-3'>
-            <UserDetails />
-            <SessionDetails />
-            <OnboardingDetails />
-          </div>
-        </>
-      )}
-    </div>
+    <>
+      <TitleBar title={titleBar} description={titleBarDescription} />
+
+      <MessageState
+        icon={
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 24 24'
+            fill='none'
+            strokeLinecap='round'
+            strokeLinejoin='round'>
+            <path d='M0 0h24v24H0z' stroke='none' />
+            <path d='M12 3l8 4.5v9L12 21l-8-4.5v-9L12 3M12 12l8-4.5M12 12v9M12 12L4 7.5' />
+          </svg>
+        }
+        title={messageStateTitle}
+        description={
+          <span>
+            This is the description of the message state with a code snippet:{" "}
+            <code className='bg-secondary text-secondary-foreground'>your-code-snippet</code>.
+          </span>
+        }
+        button={
+          <a className={buttonVariants({ size: "lg" })} href='https://nextjs-boilerplate.com/pro-saas-starter-kit'>
+            {messageStateButton}
+          </a>
+        }
+      />
+    </>
   )
 }
+
+export default DashboardIndexPage
---
+++ components/dashboard/dashboard-header.tsx
@@ -0,0 +1,98 @@
+﻿"use client"
+
+import Link from "next/link"
+// import { Logo } from "@/templates/Logo"
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs"
+
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+
+import { ActiveLink } from "../active-link"
+import { ToggleMenuButton } from "../toggle-menu-button"
+
+const DashboardHeader = (props: {
+  menu: {
+    href: string
+    label: string
+  }[]
+}) => {
+  return (
+    <>
+      <div className='flex items-center'>
+        <Link href='/dashboard' className='max-sm:hidden'>
+          {/* <Logo /> */}
+          Logo
+        </Link>
+
+        <svg
+          className='ml-1 size-8 stroke-muted-foreground max-sm:hidden'
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 24 24'
+          fill='none'
+          strokeLinecap='round'
+          strokeLinejoin='round'>
+          <path stroke='none' d='M0 0h24v24H0z' />
+          <path d='M17 5 7 19' />
+        </svg>
+
+        <OrganizationSwitcher
+          organizationProfileMode='navigation'
+          organizationProfileUrl='/dashboard/organization-profile'
+          afterCreateOrganizationUrl='/dashboard'
+          hidePersonal
+          skipInvitationScreen
+          appearance={{
+            elements: {
+              organizationSwitcherTrigger: "max-w-52",
+            },
+          }}
+        />
+
+        <nav className='ml-3 max-lg:hidden'>
+          <ul className='flex flex-row items-center gap-x-3 text-lg font-medium [&_a:hover]:opacity-100 [&_a]:opacity-75'>
+            {props.menu.map((item) => (
+              <li key={item.href}>
+                <ActiveLink href={item.href}>{item.label}</ActiveLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+
+      <div>
+        <ul className='flex items-center gap-x-1 [&_li:not(:last-child):hover]:opacity-100 [&_li:not(:last-child)]:opacity-60'>
+          <li>
+            <div className='lg:hidden'>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <ToggleMenuButton />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  {props.menu.map((item) => (
+                    <DropdownMenuItem key={item.href} asChild>
+                      <Link href={item.href}>{item.label}</Link>
+                    </DropdownMenuItem>
+                  ))}
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </li>
+
+          <li>
+            <UserButton
+              userProfileMode='navigation'
+              userProfileUrl='/dashboard/user-profile'
+              afterSignOutUrl='/'
+              appearance={{
+                elements: {
+                  rootBox: "px-2 py-1.5",
+                },
+              }}
+            />
+          </li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export { DashboardHeader }
