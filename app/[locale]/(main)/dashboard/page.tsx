1a0d1d0f4ff2d116f95cca8317f219d20c03e7fc
Nasrullah <102759013+itsNas@users.noreply.github.com>
2024-07-29 23:33:14 +0800

    add locale for dashboard

--- app/[locale]/(main)/dashboard/page.tsx
+++ app/[locale]/(main)/dashboard/page.tsx
@@ -1,17 +1,15 @@
-﻿import { buttonVariants } from "@/components/ui/button"
+﻿import { useTranslations } from "next-intl"
+
+import { buttonVariants } from "@/components/ui/button"
 import { MessageState } from "@/components/dashboard/message-state"
 import { TitleBar } from "@/components/dashboard/title-bar"
 
 const DashboardIndexPage = () => {
-  const titleBar = "Dashboard Title" // Replace with your static title
-  const titleBarDescription = "This is the description of the title bar." // Replace with your static description
-  const messageStateTitle = "Message State Title" // Replace with your static message state title
-  const messageStateDescription = "This is the description of the message state." // Replace with your static message state description
-  const messageStateButton = "Learn More" // Replace with your static button text
+  const t = useTranslations("DashboardIndex")
 
   return (
     <>
-      <TitleBar title={titleBar} description={titleBarDescription} />
+      <TitleBar title={t("title_bar")} description={t("title_bar_description")} />
 
       <MessageState
         icon={
@@ -25,16 +23,13 @@ const DashboardIndexPage = () => {
             <path d='M12 3l8 4.5v9L12 21l-8-4.5v-9L12 3M12 12l8-4.5M12 12v9M12 12L4 7.5' />
           </svg>
         }
-        title={messageStateTitle}
-        description={
-          <span>
-            This is the description of the message state with a code snippet:{" "}
-            <code className='bg-secondary text-secondary-foreground'>your-code-snippet</code>.
-          </span>
-        }
+        title={t("message_state_title")}
+        description={t.rich("message_state_description", {
+          code: (chunks) => <code className='bg-secondary text-secondary-foreground'>{chunks}</code>,
+        })}
         button={
           <a className={buttonVariants({ size: "lg" })} href='https://nextjs-boilerplate.com/pro-saas-starter-kit'>
-            {messageStateButton}
+            {t("message_state_button")}
           </a>
         }
       />
