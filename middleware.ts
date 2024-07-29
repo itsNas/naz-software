ef843ec22c3f0c66314420874549ac290d1ec0f7
Nasrullah <102759013+itsNas@users.noreply.github.com>
2024-07-29 21:37:14 +0800

    add config

--- middleware.ts
+++ middleware.ts
@@ -21,6 +21,67 @@ export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect()
 })
 
-export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-}
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// }
+
+// import { NextResponse, type NextFetchEvent, type NextRequest } from "next/server"
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
+// import createMiddleware from "next-intl/middleware"
+
+// import { AllLocales, AppConfig } from "./utils/app-config"
+
+// const intlMiddleware = createMiddleware({
+//   locales: AllLocales,
+//   localePrefix: AppConfig.localePrefix,
+//   defaultLocale: AppConfig.defaultLocale,
+// })
+
+// const isProtectedRoute = createRouteMatcher([
+//   "/dashboard(.*)",
+//   "/:locale/dashboard(.*)",
+//   "/onboarding(.*)",
+//   "/:locale/onboarding(.*)",
+// ])
+
+// export default function middleware(request: NextRequest, event: NextFetchEvent) {
+//   if (
+//     request.nextUrl.pathname.includes("/sign-in") ||
+//     request.nextUrl.pathname.includes("/sign-up") ||
+//     isProtectedRoute(request)
+//   ) {
+//     return clerkMiddleware((auth, req) => {
+//       const authObj = auth()
+
+//       if (isProtectedRoute(req)) {
+//         const locale = req.nextUrl.pathname.match(/(\/.*)\/dashboard/)?.at(1) ?? ""
+
+//         const signInUrl = new URL(`${locale}/sign-in`, req.url)
+
+//         authObj.protect({
+//           // `unauthenticatedUrl` is needed to avoid error: "Unable to find `next-intl` locale because the middleware didn't run on this request"
+//           unauthenticatedUrl: signInUrl.toString(),
+//         })
+//       }
+
+//       if (
+//         authObj.userId &&
+//         !authObj.orgId &&
+//         req.nextUrl.pathname.includes("/dashboard") &&
+//         !req.nextUrl.pathname.endsWith("/organization-selection")
+//       ) {
+//         const orgSelection = new URL("/onboarding/organization-selection", req.url)
+
+//         return NextResponse.redirect(orgSelection)
+//       }
+
+//       return intlMiddleware(req)
+//     })(request, event)
+//   }
+
+//   return intlMiddleware(request)
+// }
+
+// export const config = {
+//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// }
